2d1a13aa1d27df67336e6ebf310787f89ae27bae
DanielvV <daniel@qaleido.nl>
2017-06-13 16:13:47 +0200

    Add .my.cnf to mysqlquery function

--- migratie.sh
+++ migratie.sh
@@ -20,7 +20,7 @@ function mysqlquery () {
         -u $user \
         -p$pass \
         $database \
-        --defaults-extra-file /root/.my.cnf \
+        --defaults-file /root/.my.cnf \
         -e "$1;" \
         2>&1 | grep -v 'Using a password on the command line interface can be insecure.'
 }
